b7bc3c11f1f8bcd4f666700e21137a19234fa663
Priig <mprigmore1@gmail.com>
2014-02-28 00:22:55 -0800

    The Truth be known
    A message to Justin

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+/*
+Michael Prigmore
+February 27th, 2014
+
+Some bullshit stuff
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "You have a small penis!\n\n";
+
+	cout << endl;
+	system("Pause");
+	return 0;
+}
